8ee4a7d37d853d984a9cb84764a415762bd676a9
Den <zgibex333@gmail.com>
2021-12-15 21:44:29 +0300

    60% first course

--- algorithmsDataStructures/otherTasks.js
+++ algorithmsDataStructures/otherTasks.js
@@ -283,34 +283,34 @@ function fun2() {
   console.log(output);
 }
 
-// task 
+// task
 
 function myLocalScope() {
-    // Only change code below this line
-    const myVar = 'qwe'; 
-    console.log('inside myLocalScope', myVar);
-  }
-  myLocalScope();
-  
-  // Run and check the console
-  // myVar is not defined outside of myLocalScope
-  console.log('outside myLocalScope', myVar);
+  // Only change code below this line
+  const myVar = "qwe";
+  console.log("inside myLocalScope", myVar);
+}
+myLocalScope();
 
-//   task 
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log("outside myLocalScope", myVar);
+
+//   task
 
 // Setup
 const outerWear = "T-Shirt";
 
 function myOutfit() {
   // Only change code below this line
-  const outerWear = "sweater"
+  const outerWear = "sweater";
   // Only change code above this line
   return outerWear;
 }
 
 myOutfit();
 
-// task 
+// task
 
 // Setup
 let sum = 0;
@@ -322,14 +322,14 @@ function addThree() {
 // Only change code below this line
 
 function addFive() {
-  sum+=5; 
+  sum += 5;
 }
 // Only change code above this line
 
 addThree();
 addFive();
 
-// task 
+// task
 
 // Setup
 let processed = 0;
@@ -338,8 +338,182 @@ function processArg(num) {
   return (num + 3) / 5;
 }
 
-processed = processArg(7)
+processed = processArg(7);
 
 // Only change code below this line
 
+// task
 
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  const result = arr.shift();
+  return result;
+  // Only change code above this line
+}
+
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+// task
+
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
+
+//   task
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  return wasThatTrue ? "Yes, that was true" : "No, that was false";
+
+  // Only change code above this line
+}
+
+//   task
+
+// Setup
+function testEqual(val) {
+  if (val == 12) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+//   task
+
+// Setup
+function testStrict(val) {
+  if (val === 7) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+
+//   task
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+//   task
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) {
+    // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+//   task
+
+// Setup
+function testStrictNotEqual(val) {
+  if (val !== 17) {
+    // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+
+//   task
+
+function testGreaterThan(val) {
+  if (val > 100) {
+    // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+// task
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
+    // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {
+    // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
+//   task
+
+function testLessThan(val) {
+  if (val < 25) {
+    // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {
+    // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+//   task
+
+function testLessOrEqual(val) {
+  if (val <= 12) {
+    // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {
+    // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+//   task
