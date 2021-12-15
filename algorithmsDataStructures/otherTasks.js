afd57f9e5d7c280988247a78ce80718e942a671b
Den <zgibex333@gmail.com>
2021-12-15 18:56:59 +0300

    40% first course

--- algorithmsDataStructures/otherTasks.js
+++ algorithmsDataStructures/otherTasks.js
@@ -77,7 +77,7 @@ const someAdjective = "hard enough";
 let myStr = "Learning to code is ";
 myStr += someAdjective;
 
-// task 
+// task
 
 // Setup
 let lastNameLength = 0;
@@ -86,7 +86,7 @@ const lastName = "Lovelace";
 // Only change code below this line
 lastNameLength = lastName.length;
 
-// task 
+// task
 
 // Setup
 let firstLetterOfLastName = "";
@@ -95,7 +95,7 @@ const lastName = "Lovelace";
 // Only change code below this line
 firstLetterOfLastName = lastName[0]; // Change this line
 
-// task 
+// task
 
 // Setup
 let myStr = "Jello World";
@@ -103,4 +103,107 @@ let myStr = "Jello World";
 // Only change code below this line
 myStr = "Hello World"; // Change this line
 // Only change code above this line
+
+// task
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// task
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// task
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+// task
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =
+  "My " +
+  myNoun +
+  " is " +
+  myAdjective +
+  "." +
+  " He " +
+  myVerb +
+  " " +
+  myAdverb;
+// Only change code above this line
+
+// task
+
+// Only change code below this line
+const myArray = ["1", 1];
+
+// task
+
+// Only change code below this line
+const myArray = [["Bulls", 2], []];
+
+// task
+
+const myArray = [50, 60, 70];
+const myData = myArray[0];
+
+// task 
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+
+//  task 
+
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
+
+// task 
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3])
+
+// task 
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop(); 
+
+//  task 
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+
+const removedFromMyArray = myArray.shift()
 
