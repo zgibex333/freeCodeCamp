171864c4016d5b2181ebb89e1af39f57032c9312
Den <zgibex333@gmail.com>
2021-12-15 17:46:39 +0300

    22% first course

---
+++ algorithmsDataStructures/otherTasks.js
@@ -0,0 +1,51 @@
+// task
+const remainder = 11 % 3;
+
+// task
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// task
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+// task
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+// task
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+// task 
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+
